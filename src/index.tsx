003e5ec8f48bd59e917318df23ad8fc7856668d8
古河 和樹 <k.furukawa@snowrobin.jp>
2019-08-06 10:59:18 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, rootEl)
 })
 
+const Profiler = (React as any).Profiler as any
+
 const App: React.FC = () => {
-  return <div>Hello</div>
+  const [counter, setCounter] = React.useState(0)
+
+  const noop = React.useCallback(() => {}, [])
+
+  React.useEffect(() => {
+    const timerId = window.setInterval(() => {
+      setCounter((prev) => prev + 1)
+    }, 10)
+
+    return () => {
+      window.clearInterval(timerId)
+    }
+  }, [])
+
+  return (
+    <Profiler id="app" onRender={noop}>
+      {range(counter).map((i) => (
+        // あえて key={Math.random()}
+        <NumberComponent key={Math.random()} n={i} />
+      ))}
+    </Profiler>
+  )
+}
+
+function range(n: number): number[] {
+  return [...new Array(n).keys()]
+}
+
+const NumberComponent: React.FC<{ n: number }> = ({ n }) => {
+  return <div>{n}</div>
 }
